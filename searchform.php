0020c8c085d627dd5f59f5a37cb2d055d0501b1d
Miguel Peixe <miguel@cardume.art.br>
2013-02-18 19:37:14 -0300

    _e search button

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
     <div>
     	<input type="text" value="" name="s" id="s" placeholder="<?php _e('Search for stories', 'infoamazonia'); ?>" />
-        <input type="submit" id="searchsubmit" value="Search" />
+        <input type="submit" id="searchsubmit" value="<?php _e('Search', 'infoamazonia'); ?>" />
     </div>
 </form>
