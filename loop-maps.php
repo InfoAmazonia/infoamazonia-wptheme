c0e358f719109dc6ac5d0234aeef7e2897e91c47
Miguel Peixe <miguel@cardume.art.br>
2013-05-06 15:14:03 -0300

    add link to mapbox image on map loop

--- loop-maps.php
+++ loop-maps.php
@@ -6,7 +6,7 @@
 					<?php if(has_post_thumbnail()) {
 						the_post_thumbnail('map-thumb');
 					} else {
-						echo '<img src="' . mappress_get_mapbox_image($post->ID) . '" class="wp-post-image" />';
+						echo '<a href="' . get_permalink() .'" title="' . get_the_title() . '"><img src="' . mappress_get_mapbox_image($post->ID) . '" class="wp-post-image" /></a>';
 					} ?>
 					<header class="post-header">
 						<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
