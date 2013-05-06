523c52b37bb552498d40a9d4d6866b05ae62179f
Miguel Peixe <miguel@cardume.art.br>
2013-05-06 19:52:34 -0300

    new widget for category feeds

---
+++ inc/category-feeds-widget.php
@@ -0,0 +1,52 @@
+<?php
+class CategoryFeedsWidget extends WP_Widget {
+
+	function CategoryFeedsWidget() {
+		$widget_ops = array('classname' => 'widget_category_feeds', 'description' => __('RSS Feeds by selected categories', 'infoamazonia') );
+		$this->WP_Widget('CategoryFeedsWidget', __('Category Feed Links', 'infoamazonia'), $widget_ops);
+	}
+
+	function form($instance) {
+		$instance = wp_parse_args( (array) $instance, array( 'title' => __('RSS Feeds', 'infoamazonia'), 'categories' => '' ) );
+		$title = $instance['title'];
+		$categories = $instance['categories'];
+		?>
+		<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title', 'infoamazonia'); ?>: <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo attribute_escape($title); ?>" /></label></p>
+		<p><label for="<?php echo $this->get_field_id('categories'); ?>"><?php _e('Categories separated by comma', 'infoamazonia'); ?>: <input class="widefat" id="<?php echo $this->get_field_id('categories'); ?>" name="<?php echo $this->get_field_name('categories'); ?>" type="text" value="<?php echo attribute_escape($categories); ?>" /></label></p>
+		<?php
+	}
+
+	function update($new_instance, $old_instance) {
+		$instance = $old_instance;
+		$instance['title'] = $new_instance['title'];
+		$instance['categories'] = $new_instance['categories'];
+		return $instance;
+	}
+
+	function widget($args, $instance) {
+		extract($args, EXTR_SKIP);
+
+		$title = empty($instance['title']) ? ' ' : apply_filters('widget_title', __($instance['title']));
+		$categories = $instance['categories'];
+
+		echo $before_widget;
+
+		echo $before_title . $title . $after_title;
+
+		echo '<ul class="categories">';
+			if(!$categories) {
+				$category_ids = get_all_category_ids();
+			} else {
+				$category_ids = explode(',', $categories);
+			}
+			foreach($category_ids as $cat_id) {
+				echo '<li><a href="' . get_category_feed_link($cat_id) . '" title="' . __('RSS Feed', 'infoamazonia') . '" target="_blank">' . get_cat_name($cat_id) . '</a></li>';
+			}
+		echo '</ul>';
+
+		echo $after_widget;
+	}
+
+}
+add_action( 'widgets_init', create_function('', 'return register_widget("CategoryFeedsWidget");') );
+?>
