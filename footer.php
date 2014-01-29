10644769efb5ff20a1dcd66c4143d836cee70975
Miguel Peixe <miguel@cardume.art.br>
2014-01-29 17:27:22 -0200

    fix footer

--- footer.php
+++ footer.php
@@ -1,12 +1,12 @@
 	</section><!-- #main-content -->
 <footer id="colophon">
 	<ul class="nav clearfix">
-		<!-- <li class="label by">Project by</li> -->
-		<!--<li class="label supported">Supported by</li> -->
-		<li><a class="icon siej" href="http://www.siej.or.id/" target="_blank">Society of Indonesian Environmental Journalists</a></li>
-		<li><a class="icon internews" href="http://www.internews.org" target="_blank">Internews</a></li>
-		<li><a class="icon packard" href="http://www.packard.org/" target="_blank">Packard Foundation</a></li>
-		<li><a class="icon ecolab" href="http://ecolab.oeco.org.br/" target="_blank">EcoLab</a></li>
+		<li class="label by">Project by</li>
+		<li><a class="icon oeco" href="http://www.oecoamazonia.com">O Eco</a></li>
+		<li><a class="icon internews" href="http://www.internews.org">Internews</a></li>
+		<li class="label supported">Supported by</li>
+		<li><a class="icon cdkn" href="http://cdkn.org">Climate &amp; Development Knowledge Network</a></li>
+		<li><a class="icon knight" href="http://www.icfj.org/our-work/knight/fellows">International Center for Journalists</a></li>
 	</ul>
 </footer>
 <?php wp_footer(); ?>
