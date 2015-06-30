bd0e9405bb87537b2ddb001c7258262fedcb9205
Miguel Peixe <miguel@cardume.art.br>
2015-06-30 17:36:20 -0300

    start new post page with initial support to YARPP

--- single.php
+++ single.php
@@ -9,11 +9,29 @@
 					<header class="post-header">
 						<?php echo get_the_term_list($post->ID, 'publisher', '', ', ', ''); ?>
 						<h1 class="title"><?php the_title(); ?></h1>
+						<p class="buttons">
+							<a class="button" href="<?php echo get_post_meta($post->ID, 'url', true); ?>" target="_blank">
+								<span class="lsf">&#xE046;</span>
+								<?php _e('Go to the original article', 'infoamazonia'); ?>
+							</a>
+							<a class="button embed-button" href="<?php echo jeo_get_share_url(array('p' => $post->ID)); ?>" target="_blank">
+								<span class="lsf">&#xE118;</span>
+								<?php _e('Embed this story', 'infoamazonia'); ?>
+							</a>
+							<a class="button print-button" href="<?php echo jeo_get_embed_url(array('p' => $post->ID)); ?>" target="_blank">
+								<span class="lsf">&#xE10a;</span>
+								<?php _e('Print', 'infoamazonia'); ?>
+							</a>
+						</p>
+						<div class="fb-like" data-href="<?php the_permalink(); ?>" data-send="false" data-layout="button_count" data-width="450" data-show-faces="false" data-font="verdana" data-action="recommend"></div>
+						<div class="twitter-button">
+							<a href="https://twitter.com/share" class="twitter-share-button" data-via="infoamazonia" <?php if(function_exists('qtrans_getLanguage')) : ?>data-lang="<?php echo qtrans_getLanguage(); ?>"<?php endif; ?>>Tweet</a>
+						</div>
 					</header>
 					<?php if(jeo_has_marker_location()) : ?>
-						<div id="main-map" class="stage-map">
-							<?php jeo_map(); ?>
-						</div>
+						<!-- <div id="main-map" class="stage-map">
+							<?php // jeo_map(); ?>
+						</div> -->
 					<?php endif; ?>
 				</div>
 			</div>
@@ -22,27 +40,11 @@
 		<section id="content">
 			<div class="container row">
 				<div class="post-content">
-					<div class="seven columns">
+					<div class="twelve columns">
 						<div class="post-description">
 							<p class="date"><strong><?php echo get_the_date(); ?></strong></p>
 							<?php the_content(); ?>
-						</div>
-					</div>
-					<div class="five columns">
-						<?php $thumbnail = infoamazonia_get_thumbnail(); ?>
-						<div class="thumbnail">
-							<?php if($thumbnail) : ?>
-								<img src="<?php echo $thumbnail; ?>" />
-							<?php endif; ?>
-							<a class="button" href="<?php echo get_post_meta($post->ID, 'url', true); ?>" target="_blank"><?php _e('Go to the original article', 'infoamazonia'); ?></a>
-							<p class="buttons">
-								<a class="button embed-button" href="<?php echo jeo_get_share_url(array('p' => $post->ID)); ?>" target="_blank"><?php _e('Embed this story', 'infoamazonia'); ?></a>
-								<a class="button print-button" href="<?php echo jeo_get_embed_url(array('p' => $post->ID)); ?>" target="_blank"><?php _e('Print', 'infoamazonia'); ?></a>
-							</p>
-							<div class="fb-like" data-href="<?php the_permalink(); ?>" data-send="false" data-layout="button_count" data-width="450" data-show-faces="false" data-font="verdana" data-action="recommend"></div>
-							<div class="twitter-button">
-								<a href="https://twitter.com/share" class="twitter-share-button" data-via="infoamazonia" <?php if(function_exists('qtrans_getLanguage')) : ?>data-lang="<?php echo qtrans_getLanguage(); ?>"<?php endif; ?>>Tweet</a>
-							</div>
+							<?php if(function_exists('yarpp_related')) yarpp_related();?>
 						</div>
 					</div>
 				</div>
