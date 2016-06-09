6f091e81f17e0d873e2d5d8410a876263417fcf4
simgia <josego@simgia.com>
2016-06-08 23:47:24 -0400

    using project template

--- index.php
+++ index.php
@@ -3,30 +3,7 @@
 <?php 
 	global $wp;
 	if ($wp->request == 'projects') {
-?>
-
-<?php get_header(); ?>
-
-<section id="content">
-	<div id="map-archive" class="gray-page archive-page">
-		<div class="container">
-				<?php if(have_posts()) : ?>
-
-					<section id="maps" class="map-loop-section archive-list">
-						<div class="twelve columns">
-							<header class="page-header">
-								<h1><?php _e('Projects', 'infoamazonia'); ?></h1>
-							</header>
-						</div>
-						<?php get_template_part('loop', 'projects'); ?>
-					</section>
-
-				<?php endif; ?>
-		</div>
-	</div>
-</section>
-
-<?php
+		get_template_part('content', 'projects');
 	} else {
 ?>
 
