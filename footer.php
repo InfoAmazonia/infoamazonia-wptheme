7ac1f642a77e62dd395c830a9fceaf007552e061
Stefano Wrobleski <bwstefano@users.noreply.github.com>
2018-02-08 21:31:34 -0200

    add new footer logos

--- footer.php
+++ footer.php
@@ -2,13 +2,11 @@
 <footer id="colophon">
 	<ul class="nav clearfix">
 		<li class="label by">Project by</li>
-		<li><a class="icon oeco" href="http://www.oecoamazonia.com">O Eco</a></li>
-		<li><a class="icon internews" href="http://www.internews.org">Internews</a></li>
+		<li><a class="icon ejn" href="http://earthjournalism.net/">Earth Journalism Network</a></li>
 		<li class="label supported">Supported by</li>
-		<li><a class="icon cdkn" href="http://cdkn.org">Climate &amp; Development Knowledge Network</a></li>
 		<li><a class="icon knight" href="http://www.icfj.org/our-work/knight/fellows">International Center for Journalists</a></li>
-		<li><a class="icon avina" href="http://www.avina.net/">Avina</a></li>
-		<li><a class="icon skoll" href="http://www.skollfoundation.org/">Skoll Foundation</a></li>
+		<li><a class="icon act" href="http://www.amazonteam.org/">Amazon Conservation Team</a></li>
+		<li><a class="icon dejusticia" href="https://www.dejusticia.org/">Dejusticia</a></li>
 	</ul>
 </footer>
 <?php wp_footer(); ?>
